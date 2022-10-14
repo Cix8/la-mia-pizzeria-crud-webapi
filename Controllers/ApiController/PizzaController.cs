47af1f98dda12e4065ca20caf0de991c78e337fb
Cix8 <francesco.natal_fn@libero.it>
2022-10-14 17:00:41 +0200

    raw query improved in Api/PizzaController

--- Controllers/ApiController/PizzaController.cs
+++ Controllers/ApiController/PizzaController.cs
@@ -41,7 +41,7 @@ namespace la_mia_pizzeria_static.Controllers.ApiController
                 }
                 using (Pizzeria ctx = new Pizzeria())
                 {
-                    pizza.Ingredients = ctx.Ingredients.FromSqlRaw($"SELECT DISTINCT i.Id, i.Name FROM Ingredients as i INNER JOIN IngredientModelPizzaModel as ingp ON ingp.IngredientsId = i.Id INNER JOIN Pizzas as p ON ingp.PizzasId = {pizza.Id}").ToList();
+                    pizza.Ingredients = ctx.Ingredients.FromSqlRaw($"SELECT i.Id, i.Name FROM Ingredients as i INNER JOIN IngredientModelPizzaModel as ingp ON i.Id = ingp.IngredientsId INNER JOIN Pizzas as p ON ingp.PizzasId = p.Id WHERE p.Id = {pizza.Id}").ToList();
                 }
             }
             return Ok(pizzaList);
