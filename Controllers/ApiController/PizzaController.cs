8e9d21dfb4b1f709e5cd34bc0d11d907c7dd66b9
Cix8 <francesco.natal_fn@libero.it>
2022-10-14 17:27:33 +0200

    delete unnecessary context in Api/PizzaController

--- Controllers/ApiController/PizzaController.cs
+++ Controllers/ApiController/PizzaController.cs
@@ -38,9 +38,6 @@ namespace la_mia_pizzeria_static.Controllers.ApiController
                 using (Pizzeria ctx = new Pizzeria())
                 {
                     pizza.Category = ctx.Categories.Where(x => x.Id == pizza.CategoryId).FirstOrDefault();
-                }
-                using (Pizzeria ctx = new Pizzeria())
-                {
                     pizza.Ingredients = ctx.Ingredients.FromSqlRaw($"SELECT i.Id, i.Name FROM Ingredients as i INNER JOIN IngredientModelPizzaModel as ingp ON i.Id = ingp.IngredientsId INNER JOIN Pizzas as p ON ingp.PizzasId = p.Id WHERE p.Id = {pizza.Id}").ToList();
                 }
             }
