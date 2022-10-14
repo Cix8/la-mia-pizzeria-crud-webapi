578943aa45ccc112f9423ad05340f512d03342b7
Cix8 <francesco.natal_fn@libero.it>
2022-10-14 14:52:48 +0200

    add Api/PizzaController, add Home/Index_view to display axios.get_results

---
+++ Controllers/ApiController/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.MyDbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.ApiController
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private Pizzeria _pizzeria_db;
+
+        public PizzaController()
+        {
+            _pizzeria_db = new Pizzeria();
+        }
+
+        public IActionResult Get()
+        {
+            List<PizzaModel> pizzaList = _pizzeria_db.Pizzas.ToList();
+            //List<PizzaModel> pizzaList = new List<PizzaModel>();
+            return Ok(pizzaList);
+        }
+    }
+}
