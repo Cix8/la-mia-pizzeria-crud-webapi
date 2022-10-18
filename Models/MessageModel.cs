f04d5261b3d2a6a805eb7ec2d26556fb993ab62e
Cix8 <francesco.natal_fn@libero.it>
2022-10-18 16:25:04 +0200

    display validation response if user try to submit wrong inputs

--- Models/MessageModel.cs
+++ Models/MessageModel.cs
@@ -8,16 +8,16 @@ namespace la_mia_pizzeria_static.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "L'email è obbligatoria")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il nome è obbligatorio")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il titolo è obbligatorio")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il messaggio è obbligatorio")]
         [Column(TypeName = "text")]
         public string Content { get; set; }
 
